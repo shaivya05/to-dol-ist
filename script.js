752b13ac0d621813ec1f49f0c25429f2c4b689f8
shaivya05 <130532388+shaivya05@users.noreply.github.com>
2024-07-27 00:07:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputBox=document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+//function 1 this is called in html only
 function addTask()
 {
     if(inputBox.value===''){
@@ -15,6 +16,7 @@ function addTask()
     }
 inputBox.value='';
 saveData();}
+//function 2 called here only
 listContainer.addEventListener("click",function(e){
     if(e.target.tagName==="LI")
     {
@@ -27,9 +29,11 @@ listContainer.addEventListener("click",function(e){
         saveData();
     }
 },false);
+//fucntion 3 called everytime when changes made ie.when toggled and deleted..and also when added for first time
 function saveData(){
  localStorage.setItem("data",listContainer.innerHTML);
 }
+//function 4 called everytime the page loads
 function reload()
 {
     listContainer.innerHTML=localStorage.getItem("data");
